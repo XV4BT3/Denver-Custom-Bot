7f96553beb4dcfdb72c2ccba84aa062829a4e11b
XV4BT3 <142392254+XV4BT3@users.noreply.github.com>
2023-08-23 16:47:58 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,24 @@
 import discord
 from discord.ext import commands
+import asyncio
+import os
 
-bot = discord.Bot()
+client = commands.Bot(command_prefix="$", intents=discord.Intents.all())
+
+@client.event
+async def on_ready():
+    print("Hello Storm, your bot is succesfully logged in!")
+    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="$help"))
+
+async def load():
+    for filename in os.listdir('./cogs'):
+        if filename.endswith('.py'):
+            await client.load_extension(f'cogs.{filename[:-3]}')
+            print(f'{filename} is succesfully loaded!')
+
+async def main():
+    async with client:
+        await load()
+        await client.start("MTEzOTk5ODYxMzQ5NDMwMDcyNA.Gq2vdG.rT85Aq-RLxfcbBH8DzgGFT0KSa8mGqHkjxNU8k")
+
+asyncio.run(main())
---
+++ ssu.py
@@ -0,0 +1,18 @@
+import discord
+from discord.ext import commands
+from discord import app_commands
+
+class ssupy(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+
+    @commands.command()
+    async def ssu(self, ctx):
+        if ctx.author.guild_permissions.administrator or any(role.name == "testie" for role in ctx.author.roles):
+            print("User has the required role.")
+
+
+
+
+async def setup(client):
+    await client.add_cog(ssupy(client))
