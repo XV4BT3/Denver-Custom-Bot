e0ce3bd02e410f0f5ea3a62b428eb7e095ebd276
XV4BT3 <xv4bt3@gmail.com>
2023-08-24 22:26:44 +0200

    Added

---
+++ cogs/ssucommands.py
@@ -0,0 +1,29 @@
+import discord
+from discord.ext import commands
+from discord import app_commands
+
+class ssupy(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+
+    @commands.command()
+    async def ssu(self, ctx):
+        if ctx.author.guild_permissions.administrator or any(role.name == "testie" for role in ctx.author.roles):
+            channel = discord.utils.get(ctx.guild.channels, id=1039997170025234512)
+            html_color = "#56A0D3"
+            embed = discord.Embed(
+                title="Server Start Up",
+                description=f"> A server start up has started make sure to join if you reacted or get warned for not attending!  \n \n ➡ **Server Owner:** `02_xboxplayer` \n ➡ **Code:** `DENver` \n ➡ **Paste this in your browser for easier access!:** \n roblox://placeId=2534724415&launchData=%7B%22psCode%22%3A%22Denverp%22%7D \n \n **Hosted by:**  {ctx.author.mention}",
+                colour=discord.Colour(int(html_color[1:], 16)),
+            )
+            await channel.send(content = "@everyone", embed=embed)
+            message = await ctx.reply(content=f"{ctx.author.mention}, \n You've succesfully started a ssu.")
+            await message.delete(delay=5)
+            await ctx.message.delete(delay=5)
+        else:
+                message = await ctx.reply(content=f"{ctx.author.mention}, \n You've succesfully started a ssu.")
+                await message.delete(delay=5)
+                await ctx.message.delete(delay=5)
+
+async def setup(client):
+    await client.add_cog(ssupy(client))
