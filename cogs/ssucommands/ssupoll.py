c327c38815855b8ff452739b74add5358d04e8fb
XV4BT3 <xv4bt3@gmail.com>
2023-08-25 17:22:59 +0200

    Added ssupoll.py

--- cogs/ssucommands/ssupoll.py
+++ cogs/ssucommands/ssupoll.py
@@ -11,8 +11,19 @@ class ssupoll(commands.Cog):
 
     @commands.command()
     async def ssupoll(self, ctx):
-        print("Voting for a SSU.")
-
+        if ctx.author.guild_permissions.administrator or any(role.name == "testie" for role in ctx.author.roles):
+            channel = discord.utils.get(ctx.guild.channels, id = 1039997170025234512)
+            html_color = "#56A0D3"
+            embed = discord.Embed(title="SSU Vote", 
+                                description=f"> Do you want to launch an SSU? React below 3-4+ to host an SSU \n `Lets have a great roleplay session!`\n \n **Command Sent By:** {ctx.author.mention}",
+                                colour=discord.Colour(int(html_color[1:], 16)))
+            embed.set_thumbnail(url="https://media.discordapp.net/attachments/1093371346517495858/1136474512293113937/Denver_City_PNG.png?width=655&height=655")
+            embed_Sent = channel.send(content = "@everyon", embed=embed)
+            message = await ctx.reply(content=f"{ctx.author.mention}, \n You've succesfully commited a ssupoll.")
+            await message.delete(delay=5)
+            await ctx.message.delete(delay=5)
+            cursor.execute("UPDATE ssustatus SET ssu_poll_message = ? WHERE id = ?", (embed_Sent.id, 1))
+            conn.commit()
 
 async def setup(client):
     await client.add_cog(ssupoll(client))
