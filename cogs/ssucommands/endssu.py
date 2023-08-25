2eb2958b33cf4d661be308d1d125e683abf52f59
XV4BT3 <xv4bt3@gmail.com>
2023-08-25 16:57:25 +0200

    Some directory changes.

---
+++ cogs/ssucommands/endssu.py
@@ -0,0 +1,27 @@
+import discord
+from discord.ext import commands
+from discord import app_commands
+import sqlite3
+
+conn = sqlite3.connect("denver_database.db")
+cursor = conn.cursor()
+
+class endssu(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+
+    @commands.command()
+    async def endssu(self, ctx):
+        if ctx.author.guild_permissions.administrator or any(role.name == "testie" for role in ctx.author.roles):
+            cursor.execute("SELECT message_id FROM ssustatus")
+            message_Id = cursor.fetchone()
+            channel = discord.utils.get(ctx.guild.channels, id=1039997170025234512)
+            message_fetched = await channel.fetch_message(message_Id[0])
+            await message_fetched.delete()
+            await ctx.message.delete()
+            cursor.execute("UPDATE ssustatus SET message_id = NULL WHERE id = ?", (1,))
+            conn.commit()
+            conn.close
+
+async def setup(client):
+    await client.add_cog(endssu(client))
--- main.py
+++ main.py
@@ -8,18 +8,24 @@ token = os.environ.get('DISCORD_TOKEN')
 
 @client.event
 async def on_ready():
-    print("Hello Storm, your bot is succesfully logged in!")
+    print("Hello Storm, your bot is successfully logged in!")
     await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="$help"))
 
-async def load():
-    for filename in os.listdir('./cogs'):
-        if filename.endswith('.py'):
-            await client.load_extension(f'cogs.{filename[:-3]}')
-            print(f'{filename} is succesfully loaded!')
+async def load_cogs():
+    for root, dirs, files in os.walk('./cogs'):
+        for file in files:
+            if file.endswith('.py'):
+                cog_name = file[:-3]
+                cog_path = os.path.relpath(os.path.join(root, cog_name), './cogs').replace(os.path.sep, '.')
+                try:
+                    await client.load_extension(f'cogs.{cog_path}')
+                    print(f'{cog_name} is successfully loaded!')
+                except Exception as e:
+                    print(f'Failed to load {cog_name}: {e}')
 
 async def main():
     async with client:
-        await load()
+        await load_cogs()
         await client.start(token)
 
 asyncio.run(main())
